1ddc122f02f8c5942cc13cf1c9f8fc8c127507f7
Ori Sela <orisela579@gmail.com>
2025-10-31 19:03:38 +0200

    fixed ceil problem

--- Bill3.java
+++ Bill3.java
@@ -5,10 +5,11 @@ public class Bill3 {
 	    String name1 = args[0];
 		String name2 = args[1];
 		String name3 = args[2];
-	    int total = Integer.parseInt(args[3]);
+	    int total = Integer.parseInt(args[3])/3;
+		double ceiling = Math.ceil(total);
 
 		System.out.println("Dear " + name3 + ", " + name2 + ", " + name1 +
-							": pay " + total / 3.0 + " Shekels each.");
+							": pay " + ceiling+ " Shekels each.");
 	    // Replace this comment with the rest of your code   
 	}
 }
